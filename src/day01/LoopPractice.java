4a910e75624cc8a554f1d2081d839f1f3eb1f9ce
subi6519 <subi6519@gmail.com>
2020-03-01 01:15:31 -0500

    added loop practice file

---
+++ src/day01/LoopPractice.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("Counter ::" +i);
+
+        }
+
+    }
+}
