0a6081e1fc8207549fdd154344ada5eefe30bf6b
subi6519 <subi6519@gmail.com>
2020-03-12 20:00:49 -0400

    updates

--- src/day01/LoopPractice.java
+++ src/day01/LoopPractice.java
@@ -5,6 +5,8 @@ public class LoopPractice {
 
         for (int i = 0; i <10 ; i++) {
             System.out.println("Counter ::" +i);
+            System.out.println("####");
+            System.out.println("git is fun :(");
 
         }
 
